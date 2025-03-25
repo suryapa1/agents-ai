cea5178513b15f2775f65a63263d96c675dc367d
Tech Skills Transformations LLC <82771267+techupskills@users.noreply.github.com>
2025-03-24 21:37:01 -0400

    Update startOllama.sh

--- scripts/startOllama.sh
+++ scripts/startOllama.sh
@@ -1,3 +1,14 @@
 curl -fsSL https://ollama.com/install.sh | sh
-ollama pull llama3.2:latest
 ollama serve &
+pid=$!
+
+while ! pgrep -f "ollama"; do
+  sleep 0.1
+done
+
+sleep 15
+ollama pull llama3.2
+ollama pull qwen2.5:7b
+ollama list
+
+echo Ollama is ready.
